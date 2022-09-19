2706707ab2d529e3b182bcb90591ec160bd5fbec
GabrielZaffalon <gabrielzaffalon19@gmail.com>
2022-09-19 01:06:42 -0300

    first commit

--- knexfile.js
+++ knexfile.js
@@ -1,39 +1,39 @@
 export const development = {
-  client: 'better-sqlite3',
-  connection: {
-    filename: './data/revenda.db'
-  },
-  useNullAsDefault: true
-};
+    client: "better-sqlite3",
+    connection: {
+        filename: "./data/jogos.db",
+    },
+    useNullAsDefault: true,
+}
 
 export const staging = {
-  client: 'postgresql',
-  connection: {
-    database: 'my_db',
-    user: 'username',
-    password: 'password'
-  },
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    tableName: 'knex_migrations'
-  }
-};
+    client: "postgresql",
+    connection: {
+        database: "my_db",
+        user: "username",
+        password: "password",
+    },
+    pool: {
+        min: 2,
+        max: 10,
+    },
+    migrations: {
+        tableName: "knex_migrations",
+    },
+}
 
 export const production = {
-  client: 'postgresql',
-  connection: {
-    database: 'my_db',
-    user: 'username',
-    password: 'password'
-  },
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    tableName: 'knex_migrations'
-  }
-};
+    client: "postgresql",
+    connection: {
+        database: "my_db",
+        user: "username",
+        password: "password",
+    },
+    pool: {
+        min: 2,
+        max: 10,
+    },
+    migrations: {
+        tableName: "knex_migrations",
+    },
+}
